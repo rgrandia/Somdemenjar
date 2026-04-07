caace61e91efba1b98d266fb2c564f1a9718da92
Grandia <167295706+rgrandia@users.noreply.github.com>
2026-04-07 18:45:31 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    serverActions: true,
-  },
+  // Server Actions ja estan activats per defecte a Next.js 14
 }
 
 module.exports = nextConfig
